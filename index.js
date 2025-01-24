d013e66eb08deffb0f8bc772611b2482e72a29a0
HaseebRaza2004 <156467139+HaseebRaza2004@users.noreply.github.com>
2025-01-24 20:01:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://school-management-system-backend-mern.onrender.com/auth/google/callback',
+      callbackURL: '/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
